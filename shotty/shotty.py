c0455eb6f232504c0466009aef99075a12749e35
Aravind Patchigolla <paravindkumar@gmail.com>
2020-05-12 07:04:32 -0400

    snapshots will not be created when one is already in pending for a given volume.

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -16,6 +16,13 @@ def filter_instances(project):
         instances = ec2.instances.all()
 
     return instances
+
+
+def has_pending_snanpshot(volume):
+    snapshots = list(volume.snapshots.all())
+    return snapshots and snapshots[0].state == 'pending'
+
+
 @click.group()
 def cli():
     """Shotty manages snapshots"""
@@ -96,6 +103,10 @@ def create_snapshots(project):
         i.wait_until_stopped()
 
         for v in i.volumes.all():
+            if has_pending_snanpshot(v):
+                print(" Skipping {0}, snapshot already in progress.".format(v.id))
+                continue
+
             print(" Creating snapshot of {0}".format(v.id))
             v.create_snapshot(Description="Created by SnapshotAlyzer 30000")
 
